1a53caa7b089aa7898ee318e3780d1506b28792a
Vandal-William <vandal.william@hotmail.com>
2023-04-14 09:26:35 +0200

    add project and styled component

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -1,6 +1,71 @@
+import './styles.scss';
 function Project (){
     return (
-        <h1>Project</h1>
+        <main className='main-flex'>
+            <div className="card">
+                <div className="face face-front1">
+
+                </div>
+
+                <div className="face face-back">
+                    <h3 className='face-back-title'>Projet UI / UX designer</h3>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="face face-front2">
+                
+                </div>
+
+                <div className="face face-back">
+                    <h3 className='face-back-title'>Projet Runner JS</h3>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="face face-front3">
+
+                </div>
+
+                <div className="face face-back">
+                    <h3 className='face-back-title'>Projet RPG Empire of chaos</h3>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="face face-front4">
+
+                </div>
+
+                <div className="face face-back">
+                    <h3 className='face-back-title'>Projet bot Discord</h3>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="face face-front5">
+
+                </div>
+
+                <div className="face face-back">
+                    <h3 className='face-back-title'>Projet Orecipe</h3>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="face face-front6">
+
+                </div>
+
+                <div className="face face-back">
+                    <h3 className='face-back-title'>Projet API GitHub</h3>
+                </div>
+            </div>
+
+           
+
+        </main>
+
     )
 }
 
