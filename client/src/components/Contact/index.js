5f49da025cb4ddcc0140e80c00e2b2945b127f3e
Vandal-William <vandal.william@hotmail.com>
2023-04-19 14:04:31 +0200

    finish

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -4,15 +4,30 @@ function Contact (){
         <div className="container">
 
             <div className="card">
+                <h3 className='info-project'>Téléphone</h3>
+                <span className='info-info'>06.68.77.33.86</span>
 
             </div>
+
             <div className="card">
+                <h3 className='info-project'>E-mail</h3>
+                <span className='info-info'>vandal.william@hotmail.com</span>
 
             </div>
+
             <div className="card">
+                <h3 className='info-project'>Linkedin</h3>
+                <a href='https://www.linkedin.com/in/william-vandal' target='_blanc' className='info-info'>Lien Linkedin</a>
+            </div>
+
+            <div className="card">
+                <h3 className='info-project'>Github</h3>
+                <a href='https://github.com/Vandal-William' target='_blanc' className='info-info'>Lien GitHub</a>
 
             </div>
+
             <div className="card">
+               <h1 className='info-title'> Contact </h1>
 
             </div>
 
--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -9,6 +9,7 @@ function Project (){
 
                 <div className="face face-back">
                     <h3 className='face-back-title'>Projet UI / UX designer</h3>
+                    <a className="link-project" href="https://www.figma.com/file/QggVGVp1irFTfFhi7Xy6xO/Mangalaxy?node-id=3%3A2&t=js4hWstzGQmDSB9k-1" target='_blanc'>Voire le projet</a>
                 </div>
             </div>
 
@@ -19,6 +20,8 @@ function Project (){
 
                 <div className="face face-back">
                     <h3 className='face-back-title'>Projet Runner JS</h3>
+                    <a className="link-project" href="https://runnerjs.surge.sh" target='_blanc'>Jouer</a>
+                    <a className="link-project" href="https://github.com/Hetic-Project/Runner-JS" target='_blanc'>Voir le projet</a>
                 </div>
             </div>
 
@@ -29,6 +32,7 @@ function Project (){
 
                 <div className="face face-back">
                     <h3 className='face-back-title'>Projet RPG Empire of chaos</h3>
+                    <a className="link-project" href="https://github.com/Hetic-Project/Empire-Of-Chaos" target='_blanc'>Voir le projet</a>
                 </div>
             </div>
 
@@ -39,6 +43,8 @@ function Project (){
 
                 <div className="face face-back">
                     <h3 className='face-back-title'>Projet bot Discord</h3>
+                      <a className="link-project" href="https://discord.gg/AYQQyWnSdy" target='_blanc'>Jouer</a>
+                    <a className="link-project" href="https://github.com/Vandal-William/Bot-Discord" target='_blanc'>Voir le projet</a>
                 </div>
             </div>
 
@@ -49,6 +55,8 @@ function Project (){
 
                 <div className="face face-back">
                     <h3 className='face-back-title'>Projet Orecipe</h3>
+                    <a className="link-project" href="http://projets-portfolio.ovh:3011" target='_blanc'>Voir le site</a>
+                    <a className="link-project" href="https://github.com/Vandal-William/Orecipes-serveur" target='_blanc'>Voir le projet</a>
                 </div>
             </div>
 
@@ -59,6 +67,8 @@ function Project (){
 
                 <div className="face face-back">
                     <h3 className='face-back-title'>Projet API GitHub</h3>
+                      <a className="link-project" href="https://github-react.surge.sh/" target='_blanc'>Voir le site</a>
+                    <a className="link-project" href="https://github.com/Vandal-William/React-avec-github" target='_blanc'>Voir le projet</a>
                 </div>
             </div>
 
